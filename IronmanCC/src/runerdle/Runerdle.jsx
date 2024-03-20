4950fee6a270563336f7da5e4c620b8738e6ed8c
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2024-03-20 12:58:10 +0000

    decent progress on reactifying runerdle

--- IronmanCC/src/runerdle/Runerdle.jsx
+++ IronmanCC/src/runerdle/Runerdle.jsx
@@ -1,105 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import './style.css';
-import toastr from 'toastr';
+import React, { useState } from 'react';
+import settings from './images/Settings.png';
+import questionMark from './images/questionMark.png';
+import green from './images/green.png';
+import yellow from './images/yellow.png';
+import gray from './images/gray.png';
 import { GUESSES } from "./WordLists/guesses.js";
 import { WORDS } from "./WordLists/dictionary.js";
 import { HARDWORDS } from "./WordLists/hardWords.js";
-
-const NUMBER_OF_GUESSES = 6;
-const NUMBER_OF_LETTERS = 5;
-
-function Runerdle() {
-    const [board, setBoard] = useState([]);
-    const [guessesRemaining, setGuessesRemaining] = useState(NUMBER_OF_GUESSES);
-    const [currentGuess, setCurrentGuess] = useState([]);
-    const [nextLetter, setNextLetter] = useState(0);
-    const [rightGuessString, setRightGuessString] = useState([]);
-    const [rightGuessWiki, setRightGuessWiki] = useState([]);
-    const [justWords, setJustWords] = useState([]);
-    const [hardMode, setHardMode] = useState(false);
-    const [allWords, setAllWords] = useState(false);
+import Popup from 'reactjs-popup';
+import './RunerdleStyle.css';
 
 
-    useEffect(() => {
-        const handleKeyUp = (e) => {
-            if (guessesRemaining === 0) {
-                return;
-            }
+function Runerdle () {
 
-            const pressedKey = e.key;
-
-            if (pressedKey === 'Backspace' || pressedKey === 'Del') {
-                deleteLetter();
-                return;
-            }
-
-            if (pressedKey === 'Enter') {
-                checkGuess();
-                return;
-            }
-
-            const lowerKey = pressedKey.toLowerCase();
-
-            if (lowerKey.length === 1 && lowerKey.charCodeAt() >= 97 && lowerKey.charCodeAt() <= 122) {
-                insertLetter(lowerKey);
-            }
-        };
-
-        window.addEventListener('keyup', handleKeyUp);
-        return () => window.removeEventListener('keyup', handleKeyUp);
-    }, [guessesRemaining, currentGuess, nextLetter]);
-
-    function insertLetter(pressedKey) {
-        if (nextLetter < NUMBER_OF_LETTERS) {
-            const updatedGuess = [...currentGuess];
-            updatedGuess[nextLetter] = pressedKey;
-            setCurrentGuess(updatedGuess);
-            setNextLetter(nextLetter + 1);
-        }
-    }
-
-    function deleteLetter() {
-        if (nextLetter > 0) {
-            const updatedGuess = [...currentGuess];
-            updatedGuess.pop();
-            setCurrentGuess(updatedGuess);
-            setNextLetter(nextLetter - 1);
-        }
-    }
-
-    function reset() {
-        setGuessesRemaining(NUMBER_OF_GUESSES);
-        setCurrentGuess([]);
-        setNextLetter(0);
-        initBoard();
-    }
-
-    function initBoard() {
-        const initialBoard = [];
-        for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
-          let row = [];
-          for (let j = 0; j < NUMBER_OF_LETTERS; j++) {
-            row.push({ letter: '', state: 'empty' });
-          }
-          initialBoard.push(row);
-        }
-        setBoard(initialBoard);
-        getWord();
-    }
+    const [showPlayAgain, setShowPlayAgain] = useState("none");
 
     function getWord() {
-        let rand = 0;
+        let mode = GUESSES;
         if (hardMode) {
-            rand = Math.floor(Math.random() * HARDWORDS.length);
-            setRightGuessString(GUESSES[rand[0]]);
-            setRightGuessWiki(GUESSES[rand][1]);
-            setJustWords(toOneD(HARDWORDS));
-        } else {
-            rand = Math.floor(Math.random() * GUESSES.length);
-            setRightGuessString(GUESSES[rand[0]]);
-            setRightGuessWiki(GUESSES[rand][1]);
-            setJustWords(toOneD(GUESSES));
-        }
+            mode = HARDWORDS;
+        } 
+        rand = Math.floor(Math.random() * mode.length);
+        rightGuessString = mode[rand][0];
+        rightGuessWiki = mode[rand][1];
+        justWords = toOneD(mode);
     }
 
     function toOneD(twoD) {
@@ -110,194 +34,91 @@ function Runerdle() {
         return oneD;
     }
 
-  return (
-    <div>
-      <div id="game-board">
-        {renderBoard()}
-      </div>
-      <button id="playAgainButton" onClick={reset} style={{ display: 'none' }}>
-        Play Again
-      </button>
-      <div className="keyboard">
-        {/* Keyboard rendering logic */}
-      </div>
-      <div>
-            <input
-                type="checkbox"
-                id="hardModeButton"
-                checked={hardMode}
-                onChange={() => setHardMode(!hardMode)}
-            />
-            <label htmlFor="hardModeButton">Hard Mode</label>
+    function initBoard() {
+        setShowPlayAgain(false);
 
-            <input
-                type="checkbox"
-                id="dictionaryButton"
-                checked={allWords}
-                onChange={() => setAllWords(!allWords)}
-            />
-            <label htmlFor="dictionaryButton">All Words</label>
-        </div>
-    </div>
-    );
-}
-
-function checkGuess() {
-    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
-    let guessString = '';
-    let rightGuess = Array.from(rightGuessString);
-
-    for (const val of currentGuess) {
-        guessString += val;
+        let board = document.getElementById("game-board");
+    
+        for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+            let row = document.createElement("div");
+            row.className = "letter-row";
+    
+            for (let j = 0; j < 5; j++) {
+                let box = document.createElement("div");
+                box.className = "letter-box";
+                row.appendChild(box);
+            }
+    
+            board.appendChild(row);
+        }
+        getWord();
     }
 
-    if (guessString.length != 5) {
-        toastr.error("Not enough letters!");
-        return;
-    }
-
-    if (allWords) { //Dictionary is on
-        if (!justWords.includes(guessString) && !WORDS.includes(guessString)) {
-            toastr.error("Word not in list!");
+    function inputLetter(pressedKey){ 
+        if ((pressedKey === "Backspace" || pressedKey === "Del") && nextLetter !== 0) {
+            deleteLetter();
             return;
         }
-    } else {
-        if (!justWords.includes(guessString)) { //nothing is on
-            toastr.error("Word not in list!");
+
+        if (pressedKey === "Enter") {
+            checkGuess();
             return;
         }
-    }
-
-    for (let i = 0; i < 5; i++) {
-        let letterColor = '';
-        let box = row.children[i];
-        let letter = currentGuess[i];
-
-        let letterPosition = rightGuess.indexOf(currentGuess[i]);
-        // is letter in the correct guess
-        if (letterPosition === -1) {
-            letterColor = 'grey';
-        } else {
-            // now, letter is definitely in word
-            // if letter index and right guess index are the same
-            // letter is in the right position 
-            if (currentGuess[i] === rightGuess[i]) {
-                // shade green 
-                letterColor = 'green';
-            } else {
-                // shade box yellow
-                letterColor = '#d9b502';
-            }
-
-            rightGuess[letterPosition] = "#";
-        }
-
-        let delay = 250 * i;
-        setTimeout(() => {
-            //flip box
-            animateCSS(box, 'flipInX');
-            //shade box
-            box.style.backgroundColor = letterColor;
-            shadeKeyBoard(letter, letterColor);
-        }, delay)
-    }
-
-    if (guessString === rightGuessString) {
-	    
-	$("#wiki-link a").attr("href", rightGuessWiki);
-   	$("#wiki-link a").text("Your word was: " + rightGuessString);
-
-        $('#settingsModal').modal('hide');
-        setTimeout(function() {
-            $('#endScreenModal').modal('show');
-            $('#playAgainButton').show();
-        }, 1500);
-        guessesRemaining = 0;
-        return;
-    } else {
-	$("#wiki-link a").attr("href", rightGuessWiki);
-   	$("#wiki-link a").text("Your word was: " + rightGuessString);
-        guessesRemaining -= 1;
-        currentGuess = [];
-        nextLetter = 0;
-
-        if (guessesRemaining === 0) {
-            $('#settingsModal').modal('hide');
-            setTimeout(function() {
-                $('#lossScreenModal').modal('show');
-                $('#playAgainButton').show();
-            }, 1500);
-        }
-    }
-}
-
-function shadeKeyBoard(letter, color) {
-    for (const elem of document.getElementsByClassName("keyboard-button")) {
-        if (elem.textContent === letter) {
-            let oldColor = elem.style.backgroundColor;
-            if (oldColor === 'green') {
-                return;
-            }
-
-            if (oldColor === '#d9b502' && color !== 'green') {
-                return;
-            }
-
-            elem.style.backgroundColor = color;
-            break;
-        }
-    }
-}
-
-$('#hardModeButton').click(function() {
-    if (this.checked) {
-        hardMode = true;
-    } else {
-        hardMode = false;
-    }
-
-});
-
-$('#dictionaryButton').click(function() {
-    if (this.checked) {
-        allWords = true;
-    } else {
-        allWords = false;
-    }
-});
-
-$('#newWordButton').click(function() {
-    $('#endScreenModal').modal('hide');
-    reset();
-});
-
-$('#tryAgainButton').click(function() {
-    $('#lossScreenModal').modal('hide');
-    reset();
-});
-
-$('#playAgainButton').click(function() {
-    $('#playAgainButton').hide();
-    reset();
-});
-         
-$('.keyboard-button').click(function(){
     
-    if (guessesRemaining === 0) {
-        return;
+        pressedKey = pressedKey.toLowerCase();
+        
+        if (pressedKey.length === 1 && pressedKey.charCodeAt() >= 97 && pressedKey.charCodeAt() <= 122) {
+            insertLetter(pressedKey);
+        }
+    
     }
-    
-    inputLetter(String($(this).text()));
-    
-});
 
-document.querySelectorAll("button").forEach( function(item) {
-    item.addEventListener('focus', function() {
-        this.blur();
-    })
-})
+    function insertLetter(pressedKey) {
+        // if (nextLetter === 5) {
+        //     return;
+        // }
+    
+        // let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
+        // let box = row.children[nextLetter];
+        // animateCSS(box, "pulse");
+        // box.textContent = pressedKey;
+        // box.classList.add("filled-box");
+        // currentGuess.push(pressedKey);
+        // nextLetter += 1;
+    }
 
-const animateCSS = (element, animation, prefix = 'animate__') =>
+    function deleteLetter() {
+        // let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
+        // let box = row.children[nextLetter - 1];
+        // box.textContent = "";
+        // box.classList.remove("filled-box");
+        // currentGuess.pop();
+        // nextLetter -= 1;
+    }
+
+    function checkGuess() {
+
+    }
+
+    function shadeKeyBoard(letter, color) {
+        // for (const elem of document.getElementsByClassName("keyboard-button")) {
+        //     if (elem.textContent === letter) {
+        //         let oldColor = elem.style.backgroundColor;
+        //         if (oldColor === 'green') {
+        //             return;
+        //         }
+    
+        //         if (oldColor === '#d9b502' && color !== 'green') {
+        //             return;
+        //         }
+    
+        //         elem.style.backgroundColor = color;
+        //         break;
+        //     }
+        // }
+    }
+
+    const animateCSS = (element, animation, prefix = 'animate__') =>
     // We create a Promise and return it
     new Promise((resolve, reject) => {
         const animationName = `${prefix}${animation}`;
@@ -318,5 +139,142 @@ const animateCSS = (element, animation, prefix = 'animate__') =>
             once: true
         });
     });
+
+    return(
+        <>
+        <h1> Old School Runescape Wordle </h1>
+        
+        <Popup trigger={<input type="image" src={settings} className="btn btn-primary" alt="settings" />}modal nested>
+            {close => (
+                <div className ="settingsModal">
+                    <button className="closeModal" onClick={close}> </button>
+                    <div className="settingsModalHeader"> Settings </div>
+                    <div className="settingsModalContent">
+                        <div className="runerdleSetting">
+                            <div className="text">
+                                <div className="title">Hard Mode</div>
+                                <div className="description"> Adds lesser known npcs and items to the possible word list. </div>
+                            </div>
+                            <div className="control">
+                                <label className="settingsSwitch">
+                                    <input type="checkbox" name="hardModeButton" id="hardModeButton"></input>
+                                    <span className="slider round"></span>
+                                </label>
+                            </div>
+                        </div>
+                        <div className="runerdleSetting">
+                            <div className="text">
+                                <div className="title">Use Dictionary</div>
+                                <div className="description"> Allows you to submit words not found in the osrs word list. (Ex: Radio) </div>
+                            </div>
+                            <div className="control">
+                                <label className="settingsSwitch">
+                                    <input type="checkbox" name="dictionaryButton" id="dictionaryButton"></input>
+                                    <span className="slider round"></span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </Popup>
+
+        <Popup trigger={<input type="image" src={questionMark} className="btn btn-help" alt="howtoplay"/>}modal>
+        {close => (
+                <div className ="helpModal">
+                    <button className="closeModal" onClick={close}> </button>
+                    <div className="helpModalHeader"> How To Play </div>
+                    <div className="helpModalContent">
+                        <div className="text">
+                            <div className="help">Guess the OSRS related word in six tries.</div>
+                            <div className="help">Each guess must be a five-letter word. Hit enter to submit your guess.</div>
+                            <div className="help">After each guess, the color of the tiles will change to show how close</div>
+                            <div className="help">your guess was to the word.</div>
+                            <div className="help">Green means the letter is in the word and the correct spot.</div>
+                            <div className="help"> <img src={green} alt="green"/> </div>
+                            <div className="help">Yellow means the letter is in the word but in the incorrect spot.</div>
+                            <div className="help"> <img src={yellow} alt="yellow"/> </div>
+                            <div className="help">Gray means the letter is not in the word.</div>
+                            <div className="help"> <img src={gray} alt="gray"/> </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </Popup>
+
+        {/* <Popup trigger={<input type="image" src={questionMark} className="btn btn-help" alt="howtoplay"/>}modal>
+        {close => (
+                <div className ="winModal">
+                    <button className="closeModal" onClick={close}> </button>
+                    <div className="winModalHeader"> You Won! </div>
+                    <div className="winModalContent">
+                        <h3>Gratz!</h3>
+                        <div id="wiki-link"><a href="runerdle.com" target="_blank" rel="noopener noreferrer">What does this word mean? </a> </div>
+                    </div>
+                </div>
+            )}
+        </Popup>
+
+        <Popup trigger={<input type="image" src={questionMark} className="btn btn-help" alt="howtoplay"/>}modal>
+        {close => (
+                <div className ="lossModal">
+                    <button className="closeModal" onClick={close}> </button>
+                    <div className="lossModalHeader"> You Lost! </div>
+                    <div className="lossModalContent">
+                        <h3>Maybe you should study up</h3>
+                        <div id="wiki-link"><a href="runerdle.com" target="_blank" rel="noopener noreferrer">What does this word mean? </a> </div>
+                    </div>
+                </div>
+            )}
+        </Popup> */}
+
+        <div className="game-board">
+        </div>
+
+        <div className="keyboard-cont">
+            <div className="first-row">
+                <button className="keyboard-button">q</button>
+                <button className="keyboard-button">w</button>
+                <button className="keyboard-button">e</button>
+                <button className="keyboard-button">r</button>
+                <button className="keyboard-button">t</button>
+                <button className="keyboard-button">y</button>
+                <button className="keyboard-button">u</button>
+                <button className="keyboard-button">i</button>
+                <button className="keyboard-button">o</button>
+                <button className="keyboard-button">p</button>
+            </div>
+            <div className="second-row">
+                <button className="keyboard-button">a</button>
+                <button className="keyboard-button">s</button>
+                <button className="keyboard-button">d</button>
+                <button className="keyboard-button">f</button>
+                <button className="keyboard-button">g</button>
+                <button className="keyboard-button">h</button>
+                <button className="keyboard-button">j</button>
+                <button className="keyboard-button">k</button>
+                <button className="keyboard-button">l</button>
+            </div>
+            <div className="third-row">
+                <button className="keyboard-button">Del</button>
+                <button className="keyboard-button">z</button>
+                <button className="keyboard-button">x</button>
+                <button className="keyboard-button">c</button>
+                <button className="keyboard-button">v</button>
+                <button className="keyboard-button">b</button>
+                <button className="keyboard-button">n</button>
+                <button className="keyboard-button">m</button>
+                <button className="keyboard-button">Enter</button>
+            </div>
+        </div>
+
+        <div>
+            <button style={{ display: {showPlayAgain} }}> Play Again?</button>
+        </div>
+        
+
+        </>
+    );
+}
 
 export default Runerdle;
