96bc08e125a2b7a78d2c36f0ba863aee38fc9b58
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2024-03-28 13:21:43 +0000

    adding footer/header

--- IronmanCC/src/App.jsx
+++ IronmanCC/src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Runerdle from './runerdle/Runerdle';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 import './App.css'
 
 function App() {
   return (
     <Router>
+      <Header />
+    
       <Routes>
         <Route path="/runerdle" element={<Runerdle />} />
       </Routes>
+
+      <Footer />
     </Router>
   );
 }
---
+++ IronmanCC/src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/Header.css';
+const Header = () => {
+  return (
+    <div className="header">
+      <a href="/">Did Justen Get His Tbow Yet?</a>
+      <a href="https://templeosrs.com/groups/overview.php?id=2176">Ironman CC Group Page</a>
+      <a href="/previousEvents">Previous Events</a>
+    </div>
+  );
+};
+
+export default Header;
