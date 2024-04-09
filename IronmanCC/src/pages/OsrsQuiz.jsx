2e6fa3804944166ef8786adb38b4d49e9ff6a2cb
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2024-04-09 13:07:11 +0000

    Quiz questions

---
+++ IronmanCC/src/pages/OsrsQuiz.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './css/OsrsQuiz.css';
+
+const OsrsQuiz = () => {
+
+  return (
+    <div>
+      <h1>Quiz</h1>
+    </div>
+  );
+};
+
+export default OsrsQuiz;
