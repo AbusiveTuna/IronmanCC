e2663b6df2a660a13581ff1e17025ebce042e2ed
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2024-05-28 11:36:07 +0000

    adding images and constant

---
+++ IronmanCC/src/pages/PaintEvent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './css/PaintEvent.css';
+import templeMap from '../common/constants';
+
+const PaintEvent = () => {
+  return (
+    <div>
+      <h1>No</h1>
+    </div>
+  );
+};
+
+export default PaintEvent;
