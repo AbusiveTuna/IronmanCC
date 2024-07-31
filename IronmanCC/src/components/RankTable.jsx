41c310d66eadfa8a262ccc6c677d43156adabf31
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2024-07-31 11:10:03 +0000

    Ranks

---
+++ IronmanCC/src/components/RankTable.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import ownerIcon from '../resources/ranks/Owner.webp';
+import deputyOwnerIcon from '../resources/ranks/Deputy_owner.webp';
+import administratorIcon from '../resources/ranks/Administrator.webp';
+import mod2Icon from '../resources/ranks/Brigadier.webp';
+import modIcon from '../resources/ranks/Colonel.webp';
+import retiredModIcon from '../resources/ranks/Legacy.webp';
+import vouch3Icon from '../resources/ranks/Commander.webp';
+import vouch2Icon from '../resources/ranks/Officer.webp';
+import vouch1Icon from '../resources/ranks/General.webp';
+import sergeantIcon from '../resources/ranks/Sergeant.webp';
+import corporalIcon from '../resources/ranks/Corporal.webp';
+import recruitIcon from '../resources/ranks/Recruit.webp';
+import championIcon from '../resources/ranks/Champion.webp';
+import dunceIcon from '../resources/ranks/Gnome_Child.webp';
+import guestIcon from '../resources/ranks/Guest.webp';
+
+const data = [
+    { icon: ownerIcon, text: 'Owner' },
+    { icon: deputyOwnerIcon, text: 'Deputy Owner' },
+    { icon: administratorIcon, text: 'Administrator' },
+    { icon: mod2Icon, text: 'Senior Moderator' },
+    { icon: modIcon, text: 'Moderator' },
+    { icon: retiredModIcon, text: 'Former Mod' },
+    { icon: vouch3Icon, text: 'Vouch Rank 3' },
+    { icon: vouch2Icon, text: 'Vouch Rank 2' },
+    { icon: vouch1Icon, text: 'Vouch Rank 1' },
+    { icon: sergeantIcon, text: '2200 Total lvl' },
+    { icon: corporalIcon, text: '2000 Total lvl' },
+    { icon: recruitIcon, text: '1750 Total lvl' },
+    { icon: championIcon, text: 'Under 1750 Total lvl' },
+    { icon: dunceIcon, text: 'Dunce rank ' },
+    { icon: guestIcon, text: 'Guest' },
+  ];
+
+const RankTable = () => {
+  return (
+    <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+      <tbody>
+        {data.map((item, index) => (
+          <tr key={index} style={{ borderBottom: '1px solid #ddd' }}>
+            <td style={{ padding: '3px', textAlign: 'center', width: '30px' }}>
+              <img src={item.icon} alt={`icon-${index}`} style={{ width: '15px', height: '15px' }} />
+            </td>
+            <td style={{ padding: '3px' }}>{item.text}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default RankTable;
--- IronmanCC/src/pages/WelcomePage.jsx
+++ IronmanCC/src/pages/WelcomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './css/WelcomePage.css';
 import discordIcon from '../resources/discord.png';
+import RankTable from '../components/RankTable';
 
 const WelcomePage = () => {
   return (
@@ -16,14 +17,8 @@ const WelcomePage = () => {
           <p>We are open to Irons old and new, whether you're just starting your first iron, or maxed and looking to find new friends to raid with, the IronmanCC has something for everybody.</p>
         </div>
 
-        <div className="how-to-join">
-            <h2>How To Join</h2>
-            <p>Join the <strong>IronmanCC</strong> clan chat in-game</p>
-            <p>Applications open for new members on the first of every month, and are open until we hit the in-game limit of 500 members.</p>
-            <sub>Note: Applications close whenever we hit 495 members as spots are left open to account for returning players</sub>
-        </div>
-
         <div className="discord-info">
+          <br></br>
           <h2>Join the Ironscape Discord</h2>
           <div className="discord">
             <img src={discordIcon} alt="Discord" />
@@ -31,10 +26,19 @@ const WelcomePage = () => {
           </div>
         </div>
 
+        <div className="how-to-join">
+            <h2>How To Join</h2>
+            <p>Join the <strong>IronmanCC</strong> clan chat in-game</p>
+            <p>Applications open for new members on the first of every month, and are open until we hit the in-game limit of 500 members.</p>
+            <sub>Note: Applications close whenever we hit 495 members as spots are left open to account for returning players</sub>
+            <h2>CC Information</h2>
+            <p>CC Ranks follow the table to the right.</p>
+            <p>Vouch Ranks are given for being Active, Helpful, and Friendly in the CC.</p>
+        </div>
+
         <div className="toolbar-info">
-          <h2>IronmanCC.com</h2>
-          <p>This website serves as a catch-all for all things osrs related that I develop</p>
-          <p>From clan events, to random clones of games with an OSRS twist, IronmanCC is one of the sites of all time.</p>
+          <h2>Ironman CC Ranks</h2>
+          <RankTable/>
         </div>
 
       </div>
