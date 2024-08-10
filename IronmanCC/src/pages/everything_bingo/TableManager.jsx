b117cc86429cc5e022a7c55d3c4bb22ed11c608e
AbusiveTuna <tdb00096@gmail.com>
2024-08-10 13:06:03 -0400

    fix

--- IronmanCC/src/pages/everything_bingo/TableManager.jsx
+++ IronmanCC/src/pages/everything_bingo/TableManager.jsx
@@ -5,31 +5,28 @@ import { calculateRanks } from './bingoUtils';
 const TableManager = ({ type, data, showEHP }) => {
   let headers = [];
   let dataKeys = [];
+  let rankedData;
 
   switch (type) {
     case 'players':
       if (showEHP) {
         headers = ['Place', 'Player', 'Team', 'Points', 'EHP/EHB'];
-        dataKeys = ['playerName', 'teamName', 'points', 'efficiency'];
+        dataKeys = ['playerName', 'teamName', 'points','efficiency'];
       } else {
         headers = ['Place', 'Player', 'Team', 'Points'];
         dataKeys = ['playerName', 'team', 'points'];
       }
-      data = calculateRanks(data).slice(0, 10); 
+      data = data.slice(0, 10); 
       break;
-
     case 'sheetData':
       headers = ['Place', 'Player', 'Team', 'Value', 'Points'];
       dataKeys = ['name', 'team', 'value', 'points'];
-      data = calculateRanks(data); 
+      data = calculateRanks(data);
       break;
-
     case 'skillData':
       headers = ['Place', 'Player', 'Gain', 'Team', 'Points', 'EHP/EHB'];
       dataKeys = ['playerName', 'xpGained', 'teamName', 'points', 'efficiency'];
-      data = calculateRanks(data);
       break;
-
     case 'teamTotals':
       headers = ['Place', 'Team', 'Points'];
       dataKeys = ['teamName', 'points'];
@@ -38,13 +35,11 @@ const TableManager = ({ type, data, showEHP }) => {
     case 'purpleData':
       headers = ['Place', 'Raid', 'Team', 'Purple Count', 'Points'];
       dataKeys = ['raidName', 'teamName', 'purpleCount', 'points'];
-      
       data = data.reduce((acc, raid) => {
         const sortedTeams = raid.teams.sort((a, b) => b.points - a.points);
-
         sortedTeams.forEach((team, index) => {
           acc.push({
-            rank: index + 1, 
+            rank: index + 1,
             raidName: raid.raidName,
             teamName: team.teamName,
             purpleCount: team.purpleCount,
