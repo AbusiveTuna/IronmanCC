a6719c4057495d7487fa159e647851dd28be5810
AbusiveTuna <tdb00096@gmail.com>
2024-10-12 08:25:56 -0400

    redeploying goose bingo

--- IronmanCC/src/App.jsx
+++ IronmanCC/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Runerdle from './pages/runerdle/Runerdle';
-import WelcomePage from './pages/welcome_page/WelcomePage';
-import JustenMeme from './pages/justen_meme/JustenMeme';
-import PreviousEvents from './pages/previous_events/PreviousEvents';
-import OsrsQuiz from './pages/osrs_quiz/OsrsQuiz';
+import Runerdle from './runerdle/Runerdle';
+import WelcomePage from './pages/WelcomePage';
+import JustenMeme from './pages/JustenMeme';
+import PreviousEvents from './pages/PreviousEvents';
+import OsrsQuiz from './pages/OsrsQuiz';
+import GooseBingo from './pages/everything_bingo/GooseBingo';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css'
@@ -19,6 +20,7 @@ function App() {
         <Route path="/justenTbow" element={<JustenMeme />} />
         <Route path="/previousEvents" element={<PreviousEvents/>} />
         <Route path="/runerdle" element={<Runerdle />} />
+        <Route path="/gooseBingo" element={<GooseBingo />} />
       </Routes>
 
       <Footer />
