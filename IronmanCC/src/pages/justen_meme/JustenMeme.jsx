124b1c82a16b81d31bc10396a13396a8cb93e921
AbusiveTuna <tdb00096@gmail.com>
2024-10-27 03:38:54 -0400

    more apt update

--- IronmanCC/src/pages/justen_meme/JustenMeme.jsx
+++ IronmanCC/src/pages/justen_meme/JustenMeme.jsx
@@ -1,29 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
 import './JustenMeme.css';
+import justenTbow from '/resources/justenTbow.png';
 
 const JustenMeme = () => {
-  const [playerStats, setPlayerStats] = useState({ kc: null, rank: null });
-
   useEffect(() => {
     document.body.classList.add('justen-meme-page');
-
-    const fetchPlayerStats = async () => {
-      try {
-        const response = await axios.get('https://ironmancc-89ded0fcdb2b.herokuapp.com/justenTbow'); 
-        if (response.data) {
-          setPlayerStats({
-            kc: response.data.kc,
-            rank: response.data.rank,
-          });
-        }
-      } catch (error) {
-        console.error('Error fetching player stats:', error);
-      }
-    };
-
-    fetchPlayerStats();
-
     return () => {
       document.body.classList.remove('justen-meme-page');
     };
@@ -31,16 +12,14 @@ const JustenMeme = () => {
 
   return (
     <div>
-      <h1>YES!</h1>
-      {playerStats.kc !== null && playerStats.rank !== null ? (
         <div className="justenTbowStats">
+          <p>After...</p>
+          <p>Reaching Rank 13 for Irons</p>
+          <p>147 Purples</p>
+          <p>and 900 days</p>
           <p>Justen got his tbow after 3843 Chambers on 10/26/24</p>
-          <p>Chambers of Xeric KC: {playerStats.kc}</p>
-          <p>Rank: {playerStats.rank}</p>
+          <img src={justenTbow} alt="justenTbow" />
         </div>
-      ) : (
-        <p>Loading...</p>
-      )}
     </div>
   );
 };
