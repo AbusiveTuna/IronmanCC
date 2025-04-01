f2ca81fbfc1d418610b1e34ba848a80a473a281d
AbusiveTuna <tdb00096@gmail.com>
2025-04-01 18:15:31 -0400

    asd

--- IronmanCC/src/App.jsx
+++ IronmanCC/src/App.jsx
@@ -4,6 +4,7 @@ import WelcomePage from './pages/welcome_page/WelcomePage';
 import JustenMeme from './pages/justen_meme/JustenMeme';
 import RaidBingo from './pages/raid_bingo/RaidBingo';
 import RaidBingoAdmin from './pages/raid_bingo/RaidBingoAdmin';
+import BingoBuyIns from './pages/raid_bingo/BingoBuyIns';
 import FarmingTimers from './pages/farming_timers/FarmingTimers';
 import EventHistory from './pages/event_history/EventHistory';
 import Header from './components/Header';
@@ -24,6 +25,7 @@ function App() {
           <Route path="/justenTbow" element={<JustenMeme />} />
           <Route path="/raidBingo" element={<RaidBingo />} />
           <Route path="/raidBingoAdmin" element={<RaidBingoAdmin />} />
+          <Route path="/bingoBuyIns" element={<BingoBuyIns />} />
           <Route path="/eventHistory" element={<EventHistory />} />
           <Route path="/farmingTimers" element={<FarmingTimers />} />
         </Routes>
--- IronmanCC/src/components/Header.jsx
+++ IronmanCC/src/components/Header.jsx
@@ -7,6 +7,7 @@ const Header = () => {
       <a href="/justenTbow">Did Justen Get His Tbow Yet?</a>
       <a href="/farmingTimers">Farming Timers</a>
       <a href="/raidBingo">Raid Bingo 2025</a>
+      <a href="/bingoBuyIns">RB25 Buyins/Donations</a>
       <a href="/eventHistory">Event History</a>
       <a href="https://templeosrs.com/groups/overview.php?id=2176">Ironman CC Group Page</a>
     </div>
