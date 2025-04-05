fa7c450ced384095339ca33ecdfd2bad74765cb3
AbusiveTuna <tdb00096@gmail.com>
2025-04-05 12:07:11 -0400

    good enough for now

--- IronmanCC/src/pages/raid_bingo/RaidBoard.jsx
+++ IronmanCC/src/pages/raid_bingo/RaidBoard.jsx
@@ -56,10 +56,10 @@ const RaidBoard = ({ isAdmin, teamName }) => {
   };
 
   const rowGroups = [
-    [4, 4, 4],
-    [4, 4, 2],
+    [4, 4, 4, 1],
+    [4, 4, 3],
+    [3, 3, 2],
     [3, 3, 1],
-    [3, 3],
   ];
 
   const sections = [
