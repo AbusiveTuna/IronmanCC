d0d823467c7c2015440dbe887b4fe95731e08093
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2025-05-21 11:58:28 +0000

    start

--- IronmanCC/src/App.jsx
+++ IronmanCC/src/App.jsx
@@ -4,6 +4,7 @@ import WelcomePage from './pages/welcome_page/WelcomePage';
 import JustenMeme from './pages/justen_meme/JustenMeme';
 import BingoBuyIns from './pages/buyins/BingoBuyIns';
 import FarmingTimers from './pages/farming_timers/FarmingTimers';
+import EverythingBingo from './pages/everything_bingo/EverythingBingo';
 import EventHistory from './pages/event_history/EventHistory';
 import Header from './components/header/Header';
 import Footer from './components/Footer';
@@ -21,6 +22,7 @@ function App() {
         <Routes>
           <Route path="/" element={<WelcomePage />} />
           <Route path="/justenTbow" element={<JustenMeme />} />
+          <Route path="/everythingBingo" element={<EverythingBingo />} />
           <Route path="/bingoBuyIns" element={<BingoBuyIns />} />
           <Route path="/eventHistory" element={<EventHistory />} />
           <Route path="/farmingTimers" element={<FarmingTimers />} />
--- IronmanCC/src/components/header/HeaderLinks.js
+++ IronmanCC/src/components/header/HeaderLinks.js
@@ -3,6 +3,7 @@ export const HeaderLinks = {
   "Home": "/",
   "Did Justen Get His Tbow Yet?": "/justenTbow",
   "Farming Timers": "/farmingTimers",
+  "Everything Bingo v2" : "/everythingBingo",
   "Event History": "/eventHistory",
   "Ironman CC Group Page": "https://templeosrs.com/groups/overview.php?id=2176",
 }
