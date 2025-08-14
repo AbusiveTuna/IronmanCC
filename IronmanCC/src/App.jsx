6af206cbdbd943d5a0ffea83d3e1acad2ec074e9
abusivetuna <tdb00096@gmail.com>
2025-08-14 15:27:40 -0400

    bingooo

--- IronmanCC/src/App.jsx
+++ IronmanCC/src/App.jsx
@@ -10,7 +10,6 @@ import EventHistory from 'pages/eventHistory/EventHistory';
 import BingoBuyIns from 'pages/buyins/BingoBuyIns';
 import FarmingTimers from 'pages/farmingTimers/FarmingTimers';
 import SummerBingo from 'pages/summerBingo/SummerBingo';
-import SummerBingoTimer from 'pages/summerBingo/SummerBingoTimer';
 import TeamBoard from 'pages/summerBingo/TeamBoard';
 import Draft from 'pages/draft/Draft';
 import Header from 'components/header/Header';
@@ -29,12 +28,11 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/justen-tbow" element={<JustenTbow />} />
           <Route path="/farming-timers" element={<FarmingTimers />} />
-          <Route path="/summer-bingo" element={<SummerBingoTimer />} />
           <Route path="/event-history" element={<EventHistory />} />
           <Route path="/buy-ins" element={<BingoBuyIns />} />
           <Route path="/buy-ins-admin-82699" element={<BingoBuyInsAdmin />} />
 
-          <Route path="/summer-bingo-testing" element={<SummerBingo/>} />
+          <Route path="/summer-bingo" element={<SummerBingo/>} />
           <Route path="/team-tuna-h92812" element={<TeamBoard teamName="Team Tuna"/>} />
           <Route path="/team-chkn-m02919" element={<TeamBoard teamName="Team Chkn"/>} />
           <Route path="/draft-1239090109872389a929830120398" element={<Draft></Draft>} />
