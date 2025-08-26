f0ebcb4193b81c19f01824e71150143528bb87ed
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2025-08-26 18:43:30 +0000

    updates

--- IronmanCC/src/App.jsx
+++ IronmanCC/src/App.jsx
@@ -9,14 +9,10 @@ import JustenTbow from 'pages/justenTbow/JustenTbow';
 import EventHistory from 'pages/eventHistory/EventHistory';
 import BingoBuyIns from 'pages/buyins/BingoBuyIns';
 import FarmingTimers from 'pages/farmingTimers/FarmingTimers';
-import SummerBingo from 'pages/summerBingo/SummerBingo';
-import TeamBoard from 'pages/summerBingo/TeamBoard';
-import Draft from 'pages/draft/Draft';
 import Header from 'components/header/Header';
 
 import './App.css'
 import BingoBuyInsAdmin from './pages/buyins/BingoBuyInsAdmin';
-import AdminBingo from './pages/summerBingo/AdminBingo';
 
 function App() {
 
@@ -31,13 +27,6 @@ function App() {
           <Route path="/event-history" element={<EventHistory />} />
           <Route path="/buy-ins" element={<BingoBuyIns />} />
           <Route path="/buy-ins-admin-82699" element={<BingoBuyInsAdmin />} />
-
-          <Route path="/summer-bingo" element={<SummerBingo/>} />
-          <Route path="/team-tuna-h92812" element={<TeamBoard teamName="Team Tuna"/>} />
-          <Route path="/team-chkn-m02919" element={<TeamBoard teamName="Team Chkn"/>} />
-          <Route path="/draft-1239090109872389a929830120398" element={<Draft></Draft>} />
-          <Route path="/classic-bingo-admin-a4102932" element={<AdminBingo/>}/>
-
         </Routes>
       </Router>
     </DndProvider>
